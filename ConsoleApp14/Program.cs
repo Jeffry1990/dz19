a57c2e7be149bac19f9821b6fdb11367666cfcf9
Jeffry1990 <109137757+Jeffry1990@users.noreply.github.com>
2022-07-15 13:11:00 +0300

    Add files via upload

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tryCount = 100000;
+            string randomMessage = "exit";
+            string userInput;
+
+            for (int i=0; i<tryCount; i++)
+            {
+                Console.Write("Введите любое слово для повторения или exit чтобы закрыть приложение: ");
+                userInput = Console.ReadLine();
+                if(randomMessage == userInput)
+                {
+                    Console.WriteLine("Условия выполнены. Закрываемся");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Повторите ввод");
+                }
+            }
+        }
+    }
+}
